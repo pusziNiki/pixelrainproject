3ef17f3f0503cb07986a5abcb480571939e31515
pusziNiki <nikisomogyi003@gmail.com>
2023-01-22 21:16:17 +0100

    Pixel rain project

---
+++ index.js
@@ -0,0 +1,68 @@
+const canvas = document.getElementById('my-canvas');
+const ctx = canvas.getContext('2d');
+
+const img = new Image();
+img.src = 'n.png';
+
+const brightnessArray = [];
+const particlesArray = [];
+const rgbArray = [];
+
+class Particle {
+  constructor() {
+    this.x = Math.random() * canvas.width;
+    this.y = 0;
+    this.brightness = 0;
+    this.velocity = Math.random() * 3 + 0.1;
+    this.radius = Math.random() * 1.5 + 1;
+  }
+
+  update() {
+    this.y += this.velocity;
+    if (this.y >= canvas.height) {
+      this.y = 0;
+      this.x = Math.random() * canvas.width;
+    }
+    this.brightness = brightnessArray[Math.floor(this.y - 1) * canvas.width + Math.floor(this.x)];
+  }
+
+  draw() {
+    ctx.beginPath();
+    ctx.fillStyle = rgbArray[Math.floor(this.y - 1) * canvas.width + Math.floor(this.x)];
+    ctx.arc(this.x, this.y, this.radius, 0, Math.PI * 2);
+    ctx.fill();
+  }
+}
+
+img.onload = function () {
+  canvas.width = img.width;
+  canvas.height = img.height;
+  ctx.drawImage(img, 0, 0, canvas.width, canvas.height);
+  const imgData = ctx.getImageData(0, 0, canvas.width, canvas.height);
+  console.log(imgData.data);
+
+  for (let i = 0; i < imgData.data.length; i++) {
+    const red = imgData.data[i * 4];
+    const green = imgData.data[(i * 4) + 1];
+    const blue = imgData.data[(i * 4) + 2];
+    const brightness = (red + green + blue) / 3;
+    brightnessArray.push(brightness);
+    rgbArray.push(`rgb(${red}, ${green}, ${blue})`);
+  }
+  for (let i = 0; i < 5000; i++) {
+    particlesArray.push(new Particle());
+  }
+
+  const animate = () => {
+    ctx.globalAlpha = 0.03;
+    ctx.fillStyle = 'black';
+    ctx.fillRect(0, 0, canvas.width, canvas.height);
+    particlesArray.forEach((particle) => {
+      particle.update();
+      ctx.globalAlpha = particle.brightness * 0.001;
+      particle.draw();
+    });
+    requestAnimationFrame(animate);
+  };
+  animate();
+};
